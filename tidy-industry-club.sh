6d92c363c5db481686edad88ac79f66439c93068
Duncan Hull <duncan.hull@manchester.ac.uk>
2025-05-20 05:37:35 +0100

    add graduation picture

--- tidy-industry-club.sh
+++ tidy-industry-club.sh
@@ -10,30 +10,3 @@ echo "replaced index.html"
 
 find . -name "*.html" -exec sed -I '' 's/teaching.html/teaching/g' {} +
 echo "replaced teaching.html"
-
-find . -name "*.html" -exec sed -I '' 's/employers.html/employers/g' {} +
-echo "replaced employers.html"
-
-find . -name "*.html" -exec sed -I '' 's/research.html/research/g' {} +
-echo "replaced research.html"
-
-find . -name "*.html" -exec sed -I '' 's/coding-your-future.html/coding-your-future/g' {} +
-echo "replaced coding-your-future.html"
-
-find . -name "*.html" -exec sed -I '' 's/coding-their-future.html/coding-their-future/g' {} +
-echo "replaced coding-their-future.html"
-
-find . -name "*.html" -exec sed -I '' 's/referee.html/referee/g' {} +
-echo "replaced referee.html"
-
-find . -name "*.html" -exec sed -I '' 's/contact.html/contact/g' {} +
-echo "replaced contact.html"
-
-find . -name "*.html" -exec sed -I '' 's/mastersofscience.html/mastersofscience/g' {} +
-echo "replaced mastersofscience.html"
-
-find . -name "*.html" -exec sed -I '' 's/references.html/references/g' {} +
-echo "replaced references.html"
-
-cp index.html default.htm
-echo "copied index.html to default.htm"
